ad2214ec9368bee84272614d65464231f21c6775
Dodoruu <dorian.dobler03@htl-salzburg.ac.at>
2022-03-24 10:31:24 +0100

    first change with git

--- Assets/scripts/Onbandln.cs
+++ Assets/scripts/Onbandln.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Helloworld : MonoBehaviour
+public class Onbandln : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
